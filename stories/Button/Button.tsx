43dee43fd54a75262a6e513c1c5ab67c5503412b
nykka <veronicaines.garcia@gmail.com>
2022-08-08 13:22:45 -0300

    First commit

---
+++ stories/Button/Button.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+interface ButtonProps {
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      type="button"
+      className={`rounded p-4 ${backgroundColor}`}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
