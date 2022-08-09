f4a2a77a3a85ba1f4280e233fd5448c78c0bd5b2
nykka <veronicaines.garcia@gmail.com>
2022-08-09 16:34:11 -0300

    update button story

--- stories/Button/Button.stories.tsx
+++ stories/Button/Button.stories.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-
 import { Button } from "./Button";
 
 export default {
-  title: "Example/Button",
-  component: Button,
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
+  title: "Example/Button", // Title for our storybook
+  component: Button, // Component to render
 } as ComponentMeta<typeof Button>;
 
+//👇 We create a “template” of how args map to rendering
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
+//👇 Each story then reuses that template
 export const Primary = Template.bind({});
+//👇 We pass the props to the Primary story
 Primary.args = {
   label: "Button",
   backgroundColor: "bg-blue-500",
