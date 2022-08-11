e0c47caa20108615bbf1087cac95849ea2542ad5
JairDev <devalfredomoscoso@gmail.com>
2022-08-11 13:22:47 -0400

    Create a primary style story for the component

---
+++ stories/Tag/Tag.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Tag } from "./Tag";
+
+export default {
+  title: "Example/Tag", // Title for our storybook
+  component: Tag, // Component to render
+} as ComponentMeta<typeof Tag>;
+
+//👇 We create a “template” of how args map to rendering
+const Template: ComponentStory<typeof Tag> = (args) => <Tag {...args} />;
+
+export const PrimaryTag = Template.bind({});
+
+PrimaryTag.args = {
+  label: "Programación",
+  primary: true,
+  size: "lg",
+};
