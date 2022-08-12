934be49bfdb39b00b9de60110f40ccf504855abb
JairDev <devalfredomoscoso@gmail.com>
2022-08-12 15:30:12 -0400

    Added default color style in case the property is not passed in the tag component

--- stories/Tag/Tag.tsx
+++ stories/Tag/Tag.tsx
@@ -1,21 +1,22 @@
 interface TagProps {
   label: string;
-  hidden?: boolean | undefined;
+  hidden?: boolean;
   size?: "sm" | "md" | "lg";
   style: "primary" | "secondary";
 }
 
 export const Tag = ({ label, ...props }: TagProps) => {
-  const smallSize = props.size === "sm" && "text-sm";
-  const mediumSize = props.size === "md" && "text-base";
-  const largeSize = props.size === "lg" && "text-lg";
-  const hidden = props.hidden && "opacity-0";
-  const primaryStyle = props.style === "primary" && "bg-blue-400";
-  const secondaryStyle = props.style === "secondary" && "bg-emerald-400";
+  const defaultStyle = "bg-blue-400";
+  const smallSize = props.size === "sm" ? "text-sm" : "";
+  const mediumSize = props.size === "md" ? "text-base" : "";
+  const largeSize = props.size === "lg" ? "text-lg" : "";
+  const hidden = props.hidden ? "opacity-0" : "opacity-100";
+  const primaryStyle = props.style === "primary" ? "bg-blue-400" : "";
+  const secondaryStyle = props.style === "secondary" ? "bg-emerald-400" : "";
 
   return (
     <div
-      className={`${primaryStyle} ${secondaryStyle} ${smallSize} ${mediumSize} ${largeSize} ${hidden} p-1.5 rounded-full text-center`}
+      className={`${defaultStyle} ${primaryStyle}} ${secondaryStyle} ${smallSize} ${mediumSize} ${largeSize} ${hidden} p-1.5 rounded-full text-center`}
     >
       <label>{label}</label>
     </div>
