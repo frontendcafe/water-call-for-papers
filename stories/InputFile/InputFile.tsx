d44ace27342248c11395970a5759a77cd145bb81
Erik Giovani <46170949+ErikGIovani@users.noreply.github.com>
2022-08-14 01:34:05 +0000

    Utilidad agregada y implementada para corregir inperfecciones de elementos

--- stories/InputFile/InputFile.tsx
+++ stories/InputFile/InputFile.tsx
@@ -1,3 +1,5 @@
+import ShiftBy from "../../utils/ShiftBy";
+
 interface InputFileProps {
   /**
    * The label to display above the input.
@@ -40,7 +42,9 @@ export const InputFile = ({ tabIndex, label, placeholder }: InputFileProps) => {
         >
           <path d="M2.07153 18C1.52153 18 1.05053 17.8043 0.658533 17.413C0.2672 17.021 0.0715332 16.55 0.0715332 16V2C0.0715332 1.45 0.2672 0.979 0.658533 0.587C1.05053 0.195667 1.52153 0 2.07153 0H16.0715C16.6215 0 17.0925 0.195667 17.4845 0.587C17.8759 0.979 18.0715 1.45 18.0715 2V16C18.0715 16.55 17.8759 17.021 17.4845 17.413C17.0925 17.8043 16.6215 18 16.0715 18H2.07153ZM2.07153 16H16.0715V2H2.07153V16ZM3.07153 14H15.0715L11.3215 9L8.32153 13L6.07153 10L3.07153 14Z" />
         </svg>
-        {placeholder}
+        <ShiftBy y={-2}>
+          {placeholder}
+        </ShiftBy>
       </label>
       <input type="file" id="file" className="hidden" />
       <p className="font-normal text-xs text-gray-600">1500 x 600. Max. 5MB</p>
