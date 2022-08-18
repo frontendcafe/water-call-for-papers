1d91a641ac111d577852ce11c8de0c8a8cea7b22
JairDev <devalfredomoscoso@gmail.com>
2022-08-18 14:24:44 -0400

    Create a slected history for the tabs component

--- stories/Tabs/Tabs.stories.tsx
+++ stories/Tabs/Tabs.stories.tsx
@@ -5,8 +5,16 @@ import { Tabs } from "./Tabs";
 export default {
   title: "TabsGroup/Tabs",
   component: Tabs,
+  args: {
+    selected: true,
+  },
 } as ComponentMeta<typeof Tabs>;
 
-const Template: ComponentStory<typeof Tabs> = () => <Tabs />;
+const Template: ComponentStory<typeof Tabs> = (args) => <Tabs {...args} />;
 
 export const DefaultTabs = Template.bind({});
+
+export const SelectedTabs = Template.bind({});
+SelectedTabs.args = {
+  selected: true,
+};
