20b6d7db7758f35e4d148a7e232369d4d5bb27fc
JairDev <devalfredomoscoso@gmail.com>
2022-08-19 14:01:50 -0400

    Create headlessui tab component

--- stories/Tabs/Tabs.tsx
+++ stories/Tabs/Tabs.tsx
@@ -1,16 +1,60 @@
 import { Tab } from "@headlessui/react";
+import { useState } from "react";
 
-interface TabsProps {
-  states: "active" | "disabled" | "noSelected" | "hover" | "focus";
-}
+export const Tabs = () => {
+  const [events] = useState([
+    {
+      type: "Todos",
+      title: "Todos los eventos",
+      disabled: false,
+    },
+    {
+      type: "En curso",
+      title: "Eventos en curso",
+      disabled: false,
+    },
+    {
+      type: "Guardados",
+      title: "Eventos guardados",
+      disabled: false,
+    },
+    {
+      type: "Finalizados",
+      title: "Eventos finalizados",
+      disabled: true,
+    },
+  ]);
 
-const tabsStyles = {
-  active: "border-b-4 border-cyan-700",
-  noSelected: "text-blue-300",
-  hover: "hover:bg-sky-700",
-  focus: "focus:outline-none focus:ring focus:ring-cyan-400 rounded-lg",
-};
-
-export const Tabs = ({ states = "active" }: TabsProps) => {
-  return <button className={`${tabsStyles[states]} py-2 px-4 `}>Todos</button>;
+  return (
+    <div>
+      <Tab.Group>
+        <Tab.List className="flex space-x-1">
+          {events.map(({ type, disabled }) => (
+            <Tab
+              key={type}
+              className={({ selected }) =>
+                `
+            py-2 px-4
+            focus:outline-none focus:ring focus:ring-cyan-400
+          ${
+            selected
+              ? "border-b-4 border-cyan-700"
+              : "text-blue-300 hover:bg-cyan-400 hover:text-white"
+          }
+          `
+              }
+              disabled={disabled}
+            >
+              {type}
+            </Tab>
+          ))}
+        </Tab.List>
+        <Tab.Panels className="mt-4">
+          {events.map(({ title }) => (
+            <Tab.Panel key={title}>{title}</Tab.Panel>
+          ))}
+        </Tab.Panels>
+      </Tab.Group>
+    </div>
+  );
 };
