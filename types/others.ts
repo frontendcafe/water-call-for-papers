8a80b9656a14b2172c5bdcb2fbe10cf20c07b44e
TonyMckes <tonymckes@gmail.com>
2022-08-22 07:02:36 -0400

    fix collections references types

--- types/others.ts
+++ types/others.ts
@@ -1,5 +1,16 @@
+import { CollectionReference } from "firebase/firestore";
+
 // interfaces error
 export interface Error {
   message: string;
   status: number;
+}
+
+export interface FirebaseCollectionsRefs {
+  [key: string]: CollectionReference;
+  candidates: CollectionReference;
+  events: CollectionReference;
+  organizers: CollectionReference;
+  talks: CollectionReference;
+  topics: CollectionReference;
 }
