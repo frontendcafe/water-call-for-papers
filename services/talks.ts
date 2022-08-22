1490c735138f6d7fb23af7b058a6c62979ecca6c
TonyMckes <tonymckes@gmail.com>
2022-08-22 07:00:33 -0400

    refactor move service to its respective file

--- services/talks.ts
+++ services/talks.ts
@@ -120,3 +120,25 @@ export const postTalk = async ({
 
   return { ...talkData, topics: topicsData, candidates: candidatesData };
 };
+
+export const getTalk = async (talkId: string) => {
+  const talkSnap = await getDoc(doc(collectionsRef.talks, talkId));
+
+  // TODO: Add error handling
+  if (!talkSnap.exists()) return { error: "El evento no existe!" };
+
+  const talk = talkSnap.data();
+
+  const candidatesIds: CandidateId[] = talk.candidates;
+  const topicsIds: TopicId[] = talk.topics;
+
+  talk.candidates = await getDocById(candidatesIds, collectionsRef.candidates);
+  talk.topics = await getDocById(topicsIds, collectionsRef.topics);
+
+  // TODO: Validate a code (uuid) which will be previously sent by mail.
+  // ...
+
+  delete talk.uniqueCode;
+
+  return talk;
+};
