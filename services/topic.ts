ba9d28a77aaf45ddf019ad418950a483ceb63561
LPRegen <manuel.escribano.051@gmail.com>
2022-08-25 10:31:52 -0300

    refactor: refactor service

--- services/topic.ts
+++ services/topic.ts
@@ -1,4 +1,4 @@
-import { where, query, getDocs, setDoc, doc } from "firebase/firestore";
+import { where, query, getDocs, addDoc } from "firebase/firestore";
 import { collectionsRef } from "../lib/firebase-config";
 import { Topic } from "../types/talk-types";
 
@@ -10,16 +10,17 @@ export const addTopic = (topics: Pick<Topic, "description">[]) => {
     const topicsSnap = await getDocs(q);
 
     if (topicsSnap.empty) {
-      const topicRef = doc(collectionsRef.topics);
+      const topicRef = await addDoc(collectionsRef.topics, { description });
 
-      const newTopic: Topic = { id: topicRef.id, description };
-
-      await setDoc(topicRef, newTopic);
-
-      return newTopic as Topic;
+      return { id: topicRef.id, description };
     }
 
-    return topicsSnap.docs.map((topic) => topic.data() as Topic);
+    const [topicData] = topicsSnap.docs.map((topic) => {
+      const data = topic.data();
+      return data;
+    });
+
+    return topicData;
   });
 
   return Promise.all(topicsId);
