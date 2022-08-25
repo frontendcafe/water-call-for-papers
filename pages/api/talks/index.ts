daaf6520d38abf6fb39954910ed1e65b3d04e7b5
TonyMckes <tonymckes@gmail.com>
2022-08-25 17:15:41 -0400

    add endpoint to get all talks from an event

--- pages/api/talks/index.ts
+++ pages/api/talks/index.ts
@@ -1,10 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import errorHandler, { err } from "../../../lib/error-handling";
-import { postTalk } from "../../../services/talks";
+import { getTalksFromEvent, postTalk } from "../../../services/talks";
 
 export default errorHandler(
   async (req: NextApiRequest, res: NextApiResponse) => {
-    const { body, method } = req;
+    const { body, method, query } = req;
+    const { "event-id": eventId } = req.query;
+
+    if (method === "GET") {
+      const data = await getTalksFromEvent(eventId as string, query);
+      const message = undefined;
+
+      return res.status(200).json({ data, message });
+    }
 
     if (method === "POST") {
       const data = await postTalk(body);
