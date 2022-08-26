f14de2756e80aad1cda16eb29a918bff718f4194
LPRegen <manuel.escribano.051@gmail.com>
2022-08-25 21:20:27 -0300

    merge main

--- services/candidate.ts
+++ services/candidate.ts
@@ -3,6 +3,7 @@ import { db } from "../lib/firebase-config";
 import { Candidate } from "../types/candidates-types";
 
 export async function saveCandidate(dataCandidate: Candidate) {
+  dataCandidate.id = dataCandidate.email;
   const docRef = await setDoc(
     doc(db, "candidates", dataCandidate.email),
     dataCandidate
