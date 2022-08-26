ffb3a84fd4b35457cb3de65843e3f897979a657d
LPRegen <manuel.escribano.051@gmail.com>
2022-08-25 21:30:30 -0300

    Revert "fix: resolve conflicts on file"
    This reverts commit 1a3811cb6e7db78f78e4bfdfea8a5d7681a2d1ca.

--- services/talks.ts
+++ services/talks.ts
@@ -1,88 +1,47 @@
 import {
   doc,
+  DocumentData,
   getDoc,
-  getDocs,
-  orderBy,
-  query,
-  QueryConstraint,
   setDoc,
   updateDoc,
-  where,
 } from "firebase/firestore";
 import { v4 as uuidv4 } from "uuid";
 import { collectionsRef, db } from "../lib/firebase-config";
 import { getDocById } from "../lib/helpers";
 import { Candidate, CandidateId } from "../types/candidates-types";
-import {
-  ProposalStatus,
-  TalkProposal,
-  TalkProposalId,
-  TopicId,
-} from "../types/talk-types";
-import { FilterOptions } from "../types/others";
+import { ProposalStatus, TalkProposal, TopicId } from "../types/talk-types";
 import { saveCandidate } from "./candidate";
 import { addTopic } from "./topic";
 
-export const getTalksFromEvent = async (
-  eventId: string,
-  { duration, order = "asc", status, streamed, topics = [] }: FilterOptions
-) => {
+export const getTalksFromEvent = async (eventId: string) => {
   if (!eventId) {
     throw { code: 422, message: "Se requiere el ID del evento" };
   }
-  const topicsArr = topics.toString().split(",");
-  const queryConstraints: QueryConstraint[] = [];
 
-  queryConstraints.push(where("eventId", "==", eventId));
-
-  if (duration) {
-    queryConstraints.push(where("estimatedDuration", "==", parseInt(duration)));
-  }
-  if (order) {
-    queryConstraints.push(orderBy("createdAt", order));
-  }
-  if (status) {
-    queryConstraints.push(where("status", "==", status));
-  }
-  if (streamed) {
-    queryConstraints.push(where("streamed", "==", streamed === "true"));
-  }
-  if (topics.length > 0) {
-    queryConstraints.push(where("topics", "array-contains-any", topicsArr));
+  const eventSnap = await getDoc(doc(collectionsRef.events, eventId));
+  if (!eventSnap.exists()) {
+    throw { code: 404, message: "El evento no existe!" };
   }
 
-  const q = query(collectionsRef.talks, ...queryConstraints);
-  const querySnapshot = await getDocs(q);
+  const { talks: talksIds } = eventSnap.data();
 
-  if (querySnapshot.empty) return [];
+  const talks = (await getDocById(
+    talksIds,
+    collectionsRef.talks
+  )) as DocumentData[];
 
-  const talks = querySnapshot.docs.map(async (talkDoc) => {
-    const talk = talkDoc.data();
-    const candidates = talk.candidates;
-    const topics = talk.topics;
+  talks.forEach((talk) => delete talk.uniqueCode);
 
-    talk.candidates = await getDocById(candidates, collectionsRef.candidates);
-    talk.topics = await getDocById(topics, collectionsRef.topics);
-
-    delete talk.uniqueCode;
-
-    return talk;
-  });
-
-  return Promise.all(talks);
+  return talks;
 };
 
 export const updateStatusFromTalks = async (
-  talkId: TalkProposalId,
-  { status }: Pick<TalkProposal, "status">
+  params: Pick<TalkProposal, "id" | "status">
 ) => {
   // update status from talks
-  if (!status) {
-    throw { code: 422, message: "Se requiere el estado de la propuesta" };
-  }
-  const TalksRef = doc(db, "talks", talkId);
-  await updateDoc(TalksRef, { status }).catch(() => {
-    throw { code: 404, message: `El evento no existe!` };
+  const TalksRef = doc(db, "talks", params.id);
+  await updateDoc(TalksRef, {
+    status: params.status,
   });
 };
 
@@ -134,7 +93,7 @@ export const postTalk = async ({
   return { ...talkData, topics: topicsData, candidates: candidatesData };
 };
 
-export const getTalk = async (talkId: TalkProposalId) => {
+export const getTalk = async (talkId: string) => {
   if (!talkId) {
     throw { code: 422, message: "Se requiere el ID de la propuesta de charla" };
   }
