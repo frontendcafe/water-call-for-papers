a8dbeeb76b7a1145506e2ab58aea2b69b9384dd6
JairDev <devalfredomoscoso@gmail.com>
2022-08-27 13:36:15 -0400

    Refactor and rename tabs component

---
+++ stories/TabsGroup/Tab.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Tab } from "@headlessui/react";
+import { TabsProps } from "./types";
+
+const Tabs = ({ children, disabled }: TabsProps) => {
+  return (
+    <Tab
+      className={({ selected }) =>
+        `
+  py-2 px-4
+  focus:outline-none focus:ring-2 focus:ring-blue-800 rounded-t-lg 
+${
+  selected
+    ? "bg-white text-blue-800 font-semibold"
+    : "text-slate-400 hover:bg-blue-300 hover:text-white"
+}
+`
+      }
+      disabled={disabled}
+    >
+      {children}
+    </Tab>
+  );
+};
+
+export { Tabs as Tab };
---
+++ stories/TabsGroup/TabsList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Tab } from "@headlessui/react";
+import { TypeChildren } from "./types";
+
+export const TabsList = ({ children }: TypeChildren) => {
+  return <Tab.List className="flex space-x-4 ">{children}</Tab.List>;
+};
