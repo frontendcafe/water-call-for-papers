88ae2ec058bec123652d0277734321fb819f71a1
LPRegen <manuel.escribano.051@gmail.com>
2022-08-28 00:57:39 -0300

    feat: create icon

---
+++ stories/Icons/ExpandMore.tsx
@@ -0,0 +1,18 @@
+export const ExpandMore = () => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden
+  >
+    <path
+      d="M12.3074 5.89746L7.82019 10.3846L3.33301 5.89746"
+      stroke="#111827"
+      stroke-width="1.28205"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
