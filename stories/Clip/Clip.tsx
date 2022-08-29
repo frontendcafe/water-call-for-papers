b0bb504d5ad799572d77ad20370ce582f6d406f4
JairDev <devalfredomoscoso@gmail.com>
2022-08-29 15:43:44 -0400

    Refactor handle function to copy text

--- stories/Clip/Clip.tsx
+++ stories/Clip/Clip.tsx
@@ -1,36 +1,65 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 interface ClipProps {
   clipTextValue: string;
 }
 
 export const Clip = ({ clipTextValue }: ClipProps) => {
-  const refInput = useRef<HTMLInputElement>(null);
+  const refButton = useRef<HTMLButtonElement>(null);
+  const [classNameValue, setClassNameValue] = useState("0");
+  const [messageContent, setMessageContent] = useState("Copiado");
 
   const handleClick = () => {
-    const valueToRead = refInput?.current?.value;
-    // console.log(valueToRead);
+    const valueToRead = refButton?.current?.value;
     if (valueToRead) {
       navigator.clipboard
         .writeText(valueToRead)
-        .then(() => alert(`Texto copiado ${valueToRead}`))
-        .catch((error) => alert(`Ocurrio un error ${error}`));
+        .then(() => {
+          setClassNameValue("1");
+        })
+        .catch((error) => {
+          setClassNameValue("1");
+          setMessageContent(error);
+        });
+      setTimeout(() => setClassNameValue("0"), 1000);
     }
   };
 
   return (
-    <div className="flex w-min p-6">
-      <input
-        ref={refInput}
-        className="border-2 border-red-600 rounded focus:outline-none focus:ring focus:ring-violet-300 active:bg-violet-700"
-        type="text"
-        readOnly
+    <div className="relative">
+      <span className="block mb-2 text-slate-600 font-semibold">
+        Link del evento
+      </span>
+      <button
+        ref={refButton}
+        className="flex border-2 border-black-600 rounded-lg p-2 "
+        onClick={handleClick}
         value={clipTextValue}
-      />
-      <div className="ml-6 relative">
-        <span onClick={handleClick}>icon</span>
-        <span className="absolute -left-3 -top-6">Copiado</span>
-      </div>
+        title="copiar-texto"
+      >
+        <span className="block">{clipTextValue}</span>
+        <div className="ml-6">
+          <span className="block">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="#727272"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
+              />
+            </svg>
+          </span>
+        </div>
+      </button>
+      <span className={`absolute -left-0 -bottom-8 opacity-${classNameValue}`}>
+        {messageContent}
+      </span>
     </div>
   );
 };
