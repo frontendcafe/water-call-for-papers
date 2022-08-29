c41a61b7709bdeffefa3170ed089f602d55d3bf6
LPRegen <manuel.escribano.051@gmail.com>
2022-08-29 15:25:29 -0300

    refactor: add validations

--- services/topic.ts
+++ services/topic.ts
@@ -3,7 +3,27 @@ import { collectionsRef } from "../lib/firebase-config";
 import { Topic } from "../types/talk-types";
 
 export const addTopic = (topics: Pick<Topic, "description">[]) => {
+  // Topic needs to be an array.
+  if (!Array.isArray(topics)) {
+    throw { code: 422, message: `Topics needs to be an array.` };
+  }
+
+  // Check if topic arr isn't empty
+  if (topics.length <= 0) {
+    throw { code: 422, message: "Es requerido al menos un tópico." };
+  }
+
   const topicsId = topics.map(async ({ description }) => {
+    // Check if there is a description.
+    if (!description) {
+      throw { code: 422, message: "There is no description" };
+    }
+
+    // Check if description isn't an empty string.
+    if (!description.trim()) {
+      throw { code: 422, message: "An empty string isn't valid." };
+    }
+
     const constrain = where("description", "==", description);
     const q = query(collectionsRef.topics, constrain);
 
