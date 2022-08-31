33657a710c820ab4a41e3142f6167f67def5819f
Erik Giovani <46170949+ErikGIovani@users.noreply.github.com>
2022-08-31 01:19:59 +0000

    New design modal

--- stories/Modal/Modal.stories.tsx
+++ stories/Modal/Modal.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { Button } from "../Button/Button";
+import { TextArea } from "../TextArea/TextArea";
 import { useState } from "react";
 import { Modal } from "./Modal";
 
@@ -8,16 +9,113 @@ export default {
   component: Modal,
 } as ComponentMeta<typeof Modal>;
 
-const Template: ComponentStory<typeof Modal> = () => {
+const Template: ComponentStory<typeof Modal> = (args) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <Button backgroundColor="bg-blue-500" onClick={() => setIsOpen(true)} label="Activar Modal" />
-      <Modal open={isOpen} setIsOpen={setIsOpen}>
-        <h1>Hola mundo</h1>
+      <Button variant="primary" onClick={() => setIsOpen(true)}>
+        Activar Modal
+      </Button>
+      <Modal {...args} open={isOpen} setIsOpen={setIsOpen}>
+        {args.children}
       </Modal>
     </>
   );
 };
 
-export const ModalDefault = Template.bind({});
+export const ModalEliminarEvento = Template.bind({});
+
+ModalEliminarEvento.args = {
+  size: "medium",
+  children: (
+    <>
+      <h2 className="text-left font-semibold text-xl text-gray-800">
+        ¿Desea eliminar el evento?
+      </h2>
+      <p className="text-sm text-left text-gray-600">
+        Si eleminas el evento no podrás recuperarlo más adelante.
+      </p>
+      <div className="flex justify-end gap-3">
+        <Button onClick={() => null} variant="secondary">
+          Cancelar
+        </Button>
+        <Button onClick={() => null} variant="primary">
+          Eliminar
+        </Button>
+      </div>
+    </>
+  ),
+};
+
+export const ModalPostulacionEnviada = Template.bind({});
+
+ModalPostulacionEnviada.args = {
+  size: "medium",
+  children: (
+    <>
+      <svg
+        className="h-9 w-9 m-auto mt-4 stroke-indigo-600"
+        fill="none"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+      <h2 className="text-left font-semibold text-xl text-gray-800">
+        ¡Postulación enviada!
+      </h2>
+      <p className="text-sm text-left text-gray-600">
+        ¡Muchas gracias por tu interés en participar! Puedes seguir el estado de
+        tu postulación a través del código que te enviamos por mail.
+      </p>
+      <div className="flex justify-end">
+        <Button onClick={() => null} variant="primary">
+          Ver detalle
+        </Button>
+      </div>
+    </>
+  ),
+};
+
+export const ModalNotificar = Template.bind({});
+
+ModalNotificar.args = {
+  size: "large",
+  children: (
+    <>
+      <h2 className="text-left font-semibold text-xl text-gray-800">
+        Notificar
+      </h2>
+      <div>
+        <p className="mb-1 text-base text-left text-gray-800">
+          Mensaje para el disertante
+        </p>
+        <TextArea
+          label="Mensaje para el disertante"
+          rows={7}
+          idValue="notification"
+          value="Hola, [nombre speaker]!
+          Tu propuesta [nombre propuesta] ha sido desestimada. Lo sentimos, recibimos gran cantidad de postulaciones y, en esta ocasión, decidimos elegir otras propuestas.
+          Sin embargo, te invitamos a participar como oyente del evento. En breve te compartiremos el cronograma. 
+          Te esperamos!"
+        />
+      </div>
+
+      <p className="text-left text-indigo-600 text-sm">
+        Escribe el nombre del postulante y de la propuesta entre corchetes [ ]
+        para que se completen automáticamente de acuerdo a la propuesta.
+      </p>
+
+      <div className="flex justify-end">
+        <Button onClick={() => null} variant="primary">
+          Enviar notificación
+        </Button>
+      </div>
+    </>
+  ),
+};
--- stories/Modal/Modal.tsx
+++ stories/Modal/Modal.tsx
@@ -14,10 +14,19 @@ interface ModalProps {
    * The children node.
    */
   children: ReactNode;
+
+  size: "small" | "medium" | "large";
 }
 
-export const Modal = ({ open, setIsOpen, children }: ModalProps) => {
+export const Modal = ({
+  open,
+  setIsOpen,
+  children,
+  size = "medium",
+}: ModalProps) => {
   const closeButtonRef = useRef(null);
+
+  let width;
 
   useEffect(() => {
     if (open) {
@@ -26,6 +35,12 @@ export const Modal = ({ open, setIsOpen, children }: ModalProps) => {
       document.body.style.overflow = "auto";
     }
   }, [open]);
+
+  if (size == "large") {
+    width = "max-w-[608px]";
+  } else if (size == "medium") {
+    width = "max-w-[360px]";
+  }
 
   return (
     <Transition appear show={open} as={Fragment}>
@@ -45,7 +60,7 @@ export const Modal = ({ open, setIsOpen, children }: ModalProps) => {
           leaveTo="opacity-0"
         >
           <div
-            className="fixed inset-0 bg-white bg-opacity-25"
+            className="fixed inset-0 bg-black bg-opacity-25"
             aria-hidden="true"
           />
         </Transition.Child>
@@ -61,21 +76,31 @@ export const Modal = ({ open, setIsOpen, children }: ModalProps) => {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full max-w-[407px] transform overflow-hidden rounded-xl p-4 bg-gray-500 shadow-xl transition-all">
-                <div className="w-[375px] rounded-xl bg-white">
-                  <div className="flex justify-end">
-                    <button
-                      autoFocus
-                      ref={closeButtonRef}
-                      aria-label="Cerrar"
-                      onClick={() => setIsOpen(false)}
-                      className="mt-5 mr-5 text-2xl font-normal text-gray-600"
+              <Dialog.Panel
+                className={`w-full ${width} relative transform overflow-hidden rounded-xl pt-8 pl-6 pr-6 pb-4 bg-white shadow-xl transition-all`}
+              >
+                <div className="flex justify-end">
+                  <button
+                    autoFocus
+                    ref={closeButtonRef}
+                    aria-label="Cerrar"
+                    onClick={() => setIsOpen(false)}
+                    className="absolute"
+                  >
+                    <svg
+                      className="w-5 h-5 fill-indigo-600"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
                     >
-                      X
-                    </button>
-                  </div>
-                  <div className="flex p-[15px]">{children}</div>
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
                 </div>
+                <div className="flex flex-col gap-4">{children}</div>
               </Dialog.Panel>
             </Transition.Child>
           </div>
