17cde35fb45a27e58ef26e612aa3bc20db4a416d
Ale <aledeloss@gmail.com>
2022-08-31 00:21:52 -0300

    Agrego comentario sobre placeholder que no actualiza bien

--- stories/Select/Select.tsx
+++ stories/Select/Select.tsx
@@ -76,6 +76,7 @@ const Select = ({
         <div className="relative mt-1">
           <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
             <span className="block truncate">
+              {/* Aquí se muestra siempre el placeholder (cuando hay) y no actualiza a los valores elegidos. :( */}
               {placeholder ?? selected.name}
             </span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
