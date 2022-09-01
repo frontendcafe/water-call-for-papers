8590e44fb57905304df80eba068894e56ebfd4e8
JairDev <devalfredomoscoso@gmail.com>
2022-09-01 15:23:54 -0400

    Refactor Clip component

--- stories/Clip/Clip.tsx
+++ stories/Clip/Clip.tsx
@@ -18,7 +18,7 @@ export const Clip = ({ clipTextValue }: ClipProps) => {
           setIsCopy(true);
         })
         .catch((error) => {
-          setMessageContent(error);
+          setMessageContent(`${error} al copiar`);
         });
       setTimeout(() => setIsCopy(false), 1000);
     }
@@ -31,33 +31,37 @@ export const Clip = ({ clipTextValue }: ClipProps) => {
       </span>
       <button
         ref={refButton}
-        className="flex border-2 border-black-600 rounded-lg p-2 "
+        className="flex border-2 border-black-600 rounded-xl py-4 bg-blue-600 px-28 hover:bg-blue-700 active:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"
         onClick={handleClick}
         value={clipTextValue}
         title="copiar-texto"
       >
-        <span className="block">{clipTextValue}</span>
-        <div className="ml-6">
+        <div className="">
           <span className="block">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth={1.5}
-              stroke="#727272"
+              stroke="#f8fafc"
               className="w-6 h-6"
             >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
+                d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
               />
             </svg>
           </span>
         </div>
+        <span className="block text-slate-50 font-medium ml-2">
+          Compartir enlace
+        </span>
       </button>
       {isCopy && (
-        <span className={`absolute -left-0 -bottom-8`}>{messageContent}</span>
+        <span className={`absolute -left-0 -bottom-8`}>
+          {messageContent} {clipTextValue}
+        </span>
       )}
     </div>
   );
