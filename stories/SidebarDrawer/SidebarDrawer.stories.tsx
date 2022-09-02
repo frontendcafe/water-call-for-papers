fdd513c435772e38cff296fea8ab29a9eb55fcf7
TonyMckes <tonymckes@gmail.com>
2022-09-01 22:09:17 -0400

    add dummy data as event

--- stories/SidebarDrawer/SidebarDrawer.stories.tsx
+++ stories/SidebarDrawer/SidebarDrawer.stories.tsx
@@ -1,12 +1,21 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { EventData } from "../../types/events-types";
 
 import { SidebarDrawer } from "./SidebarDrawer";
 
+const dummyData: Pick<EventData, "id" | "name">[] = [
+  { id: "evt1", name: "CMYK" },
+  { id: "evt2", name: "Frontend Cafe" },
+  { id: "evt3", name: "Service Design Club" },
+];
+
 export default {
-  title: "Components/SidebarDrawer",
+  title: "Components/Sidebar Drawer",
   component: SidebarDrawer,
   argTypes: {},
-  args: {},
+  args: {
+    events: dummyData,
+  },
 } as ComponentMeta<typeof SidebarDrawer>;
 
 const Template: ComponentStory<typeof SidebarDrawer> = (args) => (
