ad3387a89fb36fb7ce932fc45cb55e386ac5d03d
TonyMckes <tonymckes@gmail.com>
2022-09-01 20:56:07 -0400

    add text opacity styles for mobile

--- stories/SidebarDrawer/TextContainer.tsx
+++ stories/SidebarDrawer/TextContainer.tsx
@@ -9,7 +9,9 @@ export const TextContainer = ({
   truncate,
   children,
 }: TextContainerProps) => {
-  const textStyles = open ? "opacity-100" : "md:sr-only opacity-0";
+  const textStyles = open
+    ? "opacity-0 md:opacity-100"
+    : "opacity-100 md:sr-only md:opacity-0";
   const textOverflow = truncate ? "truncate max-w-[10ch]" : "";
 
   return (
