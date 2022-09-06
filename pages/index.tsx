4915abdaba7c977f7a17fe97a42d7580dd798061
Maira <hmairalucia@gmail.com>
2022-09-06 12:16:30 -0300

    Descomenté para commitear

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-// import Image from "next/image";
+import Image from "next/image";
 import Link from "next/link";
 import logo_vercel from "../public/img/powered-by-vercel.svg";
 
@@ -11,12 +11,14 @@ const Home: NextPage = () => {
         <div className="m-2 mb-1">
           <Link href="https://vercel.com/?utm_source=hifrontendcafe&utm_campaign=oss">
             <a target="_blank" rel="noopener noreferrer">
-              {/*<Image
-                src={logo_vercel}
-                alt="Powered by Vercel"
-                placeholder="blur"
-                blurDataURL={logo_vercel}
-  />*/}
+              {
+                <Image
+                  src={logo_vercel}
+                  alt="Powered by Vercel"
+                  placeholder="blur"
+                  blurDataURL={logo_vercel}
+                />
+              }
             </a>
           </Link>
         </div>
