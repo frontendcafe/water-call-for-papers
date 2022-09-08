914447694bae80d5da2160e87822916bf3af615b
JairDev <devalfredomoscoso@gmail.com>
2022-09-08 12:37:14 -0400

    Create contact team card component

---
+++ stories/Card/ContactTeamCard.tsx
@@ -0,0 +1,21 @@
+export const ContactTeamCard = () => {
+  return (
+    <div>
+      <div>
+        <h3>Juan Perez</h3>
+        <h4>Frontend Developer</h4>
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic accusamus
+        quod, nihil similique ab consequuntur veniam placeat doloribus vel
+        soluta voluptatem velit ad inventore ullam nam maxime quibusdam minima
+        ex.
+      </div>
+      <div>
+        <div>Icon</div>
+        <div>Icon</div>
+        <div>Icon</div>
+      </div>
+    </div>
+  );
+};
