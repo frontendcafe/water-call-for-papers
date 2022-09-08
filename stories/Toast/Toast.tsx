67c518005d10286ca7c4ae22d8de60c0e477b0a6
LPRegen <manuel.escribano.051@gmail.com>
2022-09-08 05:39:44 -0300

    refactor: modify interface and render method

--- stories/Toast/Toast.tsx
+++ stories/Toast/Toast.tsx
@@ -2,45 +2,35 @@ import React, { HTMLAttributes, useEffect, useState } from "react";
 import { Button } from "../Button/Button";
 import { Icon } from "../Icon/Icon";
 
-interface Content {
-  /** Toast title. */
+interface ToastProps
+  extends Pick<HTMLAttributes<HTMLDivElement>, "aria-live" | "role"> {
+  /** Toast title, mandatory. */
   title: string;
 
-  /** Toast description. */
+  /** Toast description, optional. */
   description?: string;
 
   /** Title icon. */
   icon?: React.ReactNode;
-}
 
-interface ActionButtons {
-  /** Left button */
-  leftSideBtn?: React.ReactNode;
+  /** Left action button. */
+  leftActionButton?: React.ReactNode;
 
-  /** Right button */
-  rightSideBtn?: React.ReactNode;
-}
-
-interface ToastProps
-  extends Pick<HTMLAttributes<HTMLDivElement>, "aria-live" | "role"> {
-  /** Children, text, button, link, or a combination.
-   */
-  content: Content;
-
-  /** Action buttons. */
-  actionButtons?: ActionButtons;
+  /** Right action button */
+  rightActionButton?: React.ReactNode;
 
   /** If present specify when the component will disappear. Optional value in SECONDS, default value is 10 seconds.
    */
   timer?: number;
 }
 
-// Tiene que ser anunciado por los lectores de pantalla
-
 export const Toast = ({
-  content,
-  actionButtons,
-  timer = 10000,
+  title,
+  description,
+  icon,
+  leftActionButton,
+  rightActionButton,
+  timer = 10,
 }: ToastProps) => {
   const [active, setActive] = useState(true);
 
@@ -70,17 +60,42 @@ export const Toast = ({
 
   const ButtonGroup = () => (
     <div className="flex gap-4 h-10">
-      {actionButtons?.leftSideBtn}
-      {actionButtons?.rightSideBtn}
+      {leftActionButton}
+      {rightActionButton}
     </div>
   );
 
-  const Title = ({ text }: { text: string }) => (
-    <h4 className="text-base font-semibold text-[#4B64EF]">{text}</h4>
+  const Title = ({ title }: { title: ToastProps["title"] }) => (
+    <h4 className="text-base font-semibold text-[#4B64EF]">{title}</h4>
   );
 
-  const Description = ({ text }: { text: string }) => (
-    <p className="text-sm font-normal text-[#727272]">{text}</p>
+  const Description = ({
+    description,
+  }: {
+    description: ToastProps["description"];
+  }) => <p className="text-sm font-normal text-[#727272]">{description}</p>;
+
+  const contentStyle = `flex grow text-base font-semibold ${
+    icon ? "gap-2" : "flex-col"
+  }`;
+
+  const Content = () => (
+    <div className={contentStyle}>
+      {icon ? (
+        <>
+          {icon}
+          <div>
+            <Title title={title} />
+            {description && <Description description={description} />}
+          </div>
+        </>
+      ) : (
+        <>
+          <Title title={title} />
+          {description && <Description description={description} />}
+        </>
+      )}
+    </div>
   );
 
   return (
@@ -88,18 +103,10 @@ export const Toast = ({
       {active && (
         <div
           role="alert"
-          className="flex items-center gap-4 rounded-md shadow-md bg-[#FFFFFF] border-l-[6px] border-[#4B64EF] p-3"
+          className="flex items-center gap-4 rounded-md shadow-md bg-[#FFFFFF] border-l-[6px] border-[#4B64EF] p-3 relative mt-[40vh]"
         >
-          <div className="grow text-base font-semibold flex gap-2">
-            {content.icon}
-            <div className="flex flex-col">
-              <Title text={content.title} />
-              {content.description && (
-                <Description text={content.description} />
-              )}
-            </div>
-          </div>
-          {actionButtons && <ButtonGroup />}
+          <Content />
+          {(leftActionButton || rightActionButton) && <ButtonGroup />}
           <CloseButton />
         </div>
       )}
