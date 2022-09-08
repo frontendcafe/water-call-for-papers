bd434763824b19e07e9a74a8195d1ff00f118a0f
JairDev <devalfredomoscoso@gmail.com>
2022-09-08 12:58:34 -0400

    Update about-us page

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>Acerca de CMYK</h1>
+          <button>Saber mas</button>
+        </div>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem
+          perferendis eius alias, illo tempore deserunt pariatur rerum minima.
+          Fugiat molestias veniam nulla, numquam recusandae asperiores fugit
+          voluptate nesciunt quidem esse!
+        </p>
+      </div>
+      <div>
+        <div>Card</div>
+        <div>Card</div>
+        <div>Card</div>
+        <div>Card</div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutUs;
