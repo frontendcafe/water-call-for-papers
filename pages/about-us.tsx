c5f3994eefba2148c620aec4da001c5b0e17d37b
JairDev <devalfredomoscoso@gmail.com>
2022-09-10 11:46:37 -0400

    Add styles

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -1,25 +1,83 @@
 import React from "react";
+import { Button } from "../stories/Button/Button";
+import { ContactTeamCard } from "../stories/Card/ContactTeamCard";
+
+const aboutData = [
+  {
+    name: "Water Perez",
+    rol: "Frontend Developer",
+    description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem
+  perferendis eius alias, illo tempore deserunt pariatur rerum minima.
+  Fugiat molestias veniam nulla, numquam recusandae asperiores fugit
+  voluptate nesciunt quidem esse!`,
+    social: [
+      { iconName: "github", url: "https://github.com/" },
+      { iconName: "linkedin", url: "https://www.linkedin.com/" },
+      { iconName: "link", url: "https://jairdev.netlify.app/" },
+      { iconName: "mail", url: "devalfredomoscoso@gmail.com" },
+      { iconName: "behance", url: "https://behance.net/" },
+    ],
+  },
+  {
+    name: "Juan Perez",
+    rol: "Frontend Developer",
+    description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem
+  perferendis eius alias, illo tempore deserunt pariatur rerum minima.
+  Fugiat molestias veniam nulla, numquam recusandae asperiores fugit
+  voluptate nesciunt quidem esse!`,
+    social: [
+      { iconName: "github", url: "https://github.com/" },
+      { iconName: "linkedin", url: "https://www.linkedin.com/" },
+      { iconName: "link", url: "https://jairdev.netlify.app/" },
+    ],
+  },
+  {
+    name: "Frontend Perez",
+    rol: "Frontend Developer",
+    description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem
+  perferendis eius alias, illo tempore deserunt pariatur rerum minima.
+  Fugiat molestias veniam nulla, numquam recusandae asperiores fugit
+  voluptate nesciunt quidem esse!`,
+    social: [
+      { iconName: "github", url: "https://github.com/" },
+      { iconName: "linkedin", url: "https://www.linkedin.com/" },
+      { iconName: "behance", url: "https://jairdev.netlify.app/" },
+    ],
+  },
+];
 
 const AboutUs = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <h1>Acerca de CMYK</h1>
-          <button>Saber mas</button>
+    <div className="p-10">
+      <div className="flex flex-col md:flex-row justify-between">
+        <div className="md:w-8/12">
+          <h1 className="text-4xl">Acerca de CMYK</h1>
+          <p className="my-8">
+            La iniciativa CMYK promueve el desarrollo de proyectos colaborativos
+            con el objetivo de ganar experiencia en un entorno profesional.
+            Organizada por la comunidad{" "}
+            <span className="text-blue-500">Frontend Café</span> desde el año
+            XXXX, en esta ocasión por primera vez se suma a la etapa de diseño
+            la comunidad del{" "}
+            <span className="text-blue-500">Service Design Club.</span>
+          </p>
         </div>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem
-          perferendis eius alias, illo tempore deserunt pariatur rerum minima.
-          Fugiat molestias veniam nulla, numquam recusandae asperiores fugit
-          voluptate nesciunt quidem esse!
-        </p>
+        <div>
+          <Button>Saber más</Button>
+        </div>
       </div>
-      <div>
-        <div>Card</div>
-        <div>Card</div>
-        <div>Card</div>
-        <div>Card</div>
+      <div className="mt-10">
+        <h2 className="text-3xl">Equipo</h2>
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-4">
+          {aboutData.map(({ name, rol, description, social }) => (
+            <ContactTeamCard
+              name={name}
+              rol={rol}
+              description={description}
+              social={social}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- stories/Card/ContactTeamCard.tsx
+++ stories/Card/ContactTeamCard.tsx
@@ -1,20 +1,39 @@
-export const ContactTeamCard = () => {
+import { Icon } from "@iconify/react";
+interface ContactTeamCardProps {
+  name: string;
+  rol: string;
+  description: string;
+  social: Array<{
+    iconName: string;
+    url: string;
+  }>;
+}
+
+export const ContactTeamCard = ({
+  name,
+  rol,
+  description,
+  social,
+}: ContactTeamCardProps) => {
   return (
-    <div>
+    <div className="bg-gray-100 p-6 rounded-lg max-w-md">
       <div>
-        <h3>Juan Perez</h3>
-        <h4>Frontend Developer</h4>
+        <h3 className="text-lg font-semibold">{name}</h3>
+        <h4 className="text-gray-500 font-semibold mt-2">{rol}</h4>
       </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic accusamus
-        quod, nihil similique ab consequuntur veniam placeat doloribus vel
-        soluta voluptatem velit ad inventore ullam nam maxime quibusdam minima
-        ex.
-      </div>
-      <div>
-        <div>Icon</div>
-        <div>Icon</div>
-        <div>Icon</div>
+      <div className="mt-4">{description}</div>
+      <div className="mt-12">
+        <div className="text-gray-500 font-semibold">Social</div>
+        <div className="flex flex-wrap justify-start mt-2">
+          {social.map(({ iconName, url }) => (
+            <a className="mr-8 mt-2" href={url}>
+              <Icon
+                icon={`ant-design:${iconName}-outlined`}
+                style={{ fontSize: "24px", color: "#6778FF" }}
+              />
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   );
