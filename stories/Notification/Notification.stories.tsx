fb0947fa3e3cab4a5b8b5b1595e93d7362f847b0
LPRegen <manuel.escribano.051@gmail.com>
2022-09-09 21:46:42 -0300

    refactor: prev. Toast component

---
+++ stories/Notification/Notification.stories.tsx
@@ -0,0 +1,58 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Notification } from "./Notification";
+import { Icon } from "../Icon/Icon";
+import { Button } from "../Button/Button";
+
+export default {
+  title: "Components/Notification",
+  component: Notification,
+  argTypes: {
+    children: {
+      control: "text",
+    },
+  },
+  args: {
+    title: "Die unendliche Geschichte",
+    description: "La historia interminable.",
+  },
+} as ComponentMeta<typeof Notification>;
+
+const Template: ComponentStory<typeof Notification> = (args) => (
+  <Notification {...args} />
+);
+
+export const Toast = Template.bind({});
+
+Toast.args = {
+  icon: <Icon iconName="book" className="text-[#4B64EF]" aria-hidden />,
+  timer: 10,
+  children: (
+    <>
+      <Button children="left" variant="secondary" />
+      <Button children="right" variant="secondary" size="small" />
+    </>
+  ),
+  color: "toast",
+  role: "alert",
+};
+
+export const NotificationComp = Template.bind({});
+
+NotificationComp.args = {
+  icon: <Icon iconName="book" className="text-[#4B64EF]" aria-hidden />,
+  color: "notification",
+};
+
+export const Warning = Template.bind({});
+
+Warning.args = {
+  icon: <Icon iconName="book" className="text-[#4B64EF]" aria-hidden />,
+  color: "warning",
+};
+
+export const Error = Template.bind({});
+
+Error.args = {
+  icon: <Icon iconName="book" className="text-[#4B64EF]" aria-hidden />,
+  color: "error",
+};
