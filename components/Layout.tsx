8a9f51169686da96e0d9c7f1353fd9ec512aec91
TonyMckes <tonymckes@gmail.com>
2022-09-13 18:23:00 -0400

    add background color and height

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Layout: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
-  return <div>{children}</div>;
+  return <div className="h-full bg-secondary-50">{children}</div>;
 };
 
 export default Layout;
