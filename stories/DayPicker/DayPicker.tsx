6086ed4990a965ff774b746d9dd3e7e58bb15ca5
TonyMckes <tonymckes@gmail.com>
2022-10-20 23:08:42 -0400

    refactor: styles DayPicker

--- stories/DayPicker/DayPicker.tsx
+++ stories/DayPicker/DayPicker.tsx
@@ -17,6 +17,8 @@ interface DatePickerProps {
   /** Specify maximum of days possible to the future to pick a date. Example, if today is 13/10/22, set 'untilDays' for X amount of days to the future. Default is 90 days. */
   untilDays?: number;
 
+  /** Unique ID */
+  id: string;
   /** Event onChange */
   onChange: (date: Date, event: SyntheticEvent<Event>) => void;
 }
@@ -26,34 +28,36 @@ export const DayPicker = ({
   label,
   untilDays = 90,
   onChange,
+  id,
 }: DatePickerProps) => {
   const todayDate = new Date();
 
   const containerStyles =
-    "bg-white flex items-center gap-2 px-2 py-2 mt-1 text-sm border-2 rounded-xl font-normal focus:border-2 focus:border-gray-400 disabled:border-gray-200";
+    "w-full pl-9 px-2 py-4 mt-1 border focus:outline-none focus:border-primary-900 focus:ring-1 focus:ring-primary-900 disabled:ring-gray-300 disabled:ring-1 hover:ring-primary-900 hover:ring-1 rounded-xl border-secondary-700 text-secondary-800 placeholder:text-secondary-300";
 
   return (
-    <div className="flex flex-col">
-      <label className="text-sm font-normal text-gray-800" htmlFor="datepicker">
+    <div className="relative text-secondary-900">
+      <label className="text-sm" htmlFor={id}>
         {label}
       </label>
-      <div className={containerStyles}>
-        <Icon iconName="calendar" size="medium" />
-        <DatePicker
-          id="datepicker"
-          className="w-full outline-none"
-          dateFormat="dd/MM/yyyy"
-          includeDateIntervals={[
-            {
-              start: subDays(todayDate, 1),
-              end: addDays(todayDate, untilDays),
-            },
-          ]}
-          locale="es"
-          selected={date}
-          onChange={onChange}
-        />
-      </div>
+      <Icon
+        iconName="calendar"
+        className="absolute z-10 pointer-events-none top-11 left-2 text-secondary-300"
+      />
+      <DatePicker
+        id={id}
+        className={containerStyles}
+        dateFormat="dd/MM/yyyy"
+        includeDateIntervals={[
+          {
+            start: subDays(todayDate, 1),
+            end: addDays(todayDate, untilDays),
+          },
+        ]}
+        locale="es"
+        selected={date}
+        onChange={onChange}
+      />
     </div>
   );
 };
