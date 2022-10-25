89965d482cebc18c3c5bb1d2f0d9210abf6bdce7
TonyMckes <tonymckes@gmail.com>
2022-10-25 15:33:00 -0400

    add: favicons and manifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,29 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;600&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="apple-mobile-web-app-title" content="Ola" />
+        <meta name="application-name" content="Ola" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <meta
           name="description"
           content="Con Ola puedes administrar tu próximo evento, gestionar la convocatoria de charlas, evaluarlas e informar los resultados. Todo en un mismo lugar."
