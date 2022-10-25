f538c1f1730381c0545fe4c8b75039a177db916e
TonyMckes <tonymckes@gmail.com>
2022-10-25 15:19:49 -0400

    add: title and description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,16 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;600&display=swap"
           rel="stylesheet"
         />
+        <meta
+          name="description"
+          content="Con Ola puedes administrar tu próximo evento, gestionar la convocatoria de charlas, evaluarlas e informar los resultados. Todo en un mismo lugar."
+        />
+        {/* TODO: Add title on every page */}
+        <title>
+          Ola | Con Ola puedes administrar tu próximo evento, gestionar la
+          convocatoria de charlas, evaluarlas e informar los resultados. Todo en
+          un mismo lugar.
+        </title>
       </Head>
       <body className="font-work_sans">
         <Main />
