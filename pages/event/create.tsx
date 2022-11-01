688f7ad20cc4fbf0b87b67b7248c97e9351399b4
TonyMckes <tonymckes@gmail.com>
2022-11-01 18:58:39 -0400

    add: creation of topics on submit

--- pages/event/create.tsx
+++ pages/event/create.tsx
@@ -124,7 +124,10 @@ const Create = () => {
       return;
     }
 
-    // @ts-ignore
+    const topicsArray = topicsSelected
+      ? Array.from(topicsSelected.values())
+      : [];
+
     const event: NewEventData = {
       name: data.EventName,
       type: selected.value as EventType,
@@ -140,7 +143,7 @@ const Create = () => {
       timezone:
         timeZoneSelected?.value ??
         timezones.find((tz) => tz.name.includes("Buenos Aires"))?.value!,
-      // FIXME: Missing "topics", send topics on submit too?
+      topics: topicsArray,
     };
 
     try {
