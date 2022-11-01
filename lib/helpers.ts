cc630feeac9496cd9108ec0d123cbb395e232cd0
TonyMckes <tonymckes@gmail.com>
2022-11-01 09:29:38 -0400

    fix: firebase timestamp parsing

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -39,7 +39,7 @@ export async function getDocById(
       if (key === "uniqueCode") delete docData.uniqueCode;
 
       if (key in datesTimestamps) {
-        docData[key] = new Date(docData[key]);
+        docData[key] = docData[key].toDate();
       }
 
       if (key in collectionsRef && docData[key].length > 0) {
