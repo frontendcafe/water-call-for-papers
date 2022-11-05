6f3883eb6b2ba325614b38e0905e3c8d4184a00b
TonyMckes <tonymckes@gmail.com>
2022-11-05 14:28:54 -0400

    add: schema validation

---
+++ lib/schemas.ts
@@ -0,0 +1,55 @@
+import { z } from "zod";
+import { EventStatus, EventType } from "../types/events-types";
+import { ProposalStatus } from "../types/talk-types";
+
+const processDateStrings = () => {
+  return z.preprocess((arg: unknown) => {
+    if (typeof arg == "string" || arg instanceof Date) return new Date(arg);
+  }, z.date());
+};
+
+const organizerSchema = z.object({
+  email: z.string().email(),
+  fullName: z.string(),
+});
+
+const topicsSchema = z.array(z.string().min(1));
+
+const eventSchema = z.object({
+  bannerUrl: z.string().default("/img/placeholder.png"),
+  description: z.string(),
+  endDate: processDateStrings(),
+  location: z.string(),
+  name: z.string().min(1),
+  organizers: z.array(organizerSchema),
+  proposalsEndDate: processDateStrings(),
+  proposalsStartingDate: processDateStrings(),
+  startingDate: processDateStrings(),
+  status: z.nativeEnum(EventStatus),
+  talks: z.array(z.string()).default([]),
+  timezone: z.string(),
+  topics: topicsSchema,
+  type: z.nativeEnum(EventType),
+});
+
+const candidateSchema = z.object({
+  email: z.string().email(),
+  firstName: z.string().min(1),
+  lastName: z.string().min(1),
+});
+
+const talkSchema = z.object({
+  attachments: z.array(z.string()),
+  candidates: z.array(candidateSchema),
+  estimatedDuration: z.number(),
+  eventId: z.string(),
+  status: z.string().default(ProposalStatus.EnEsperaSinAbrir),
+  streamed: z.boolean(),
+  summary: z.string(),
+  title: z.string().min(1),
+  topics: topicsSchema,
+});
+
+const talkStatusSchema = talkSchema.pick({ status: true });
+
+export { topicsSchema, eventSchema, talkSchema, talkStatusSchema };
